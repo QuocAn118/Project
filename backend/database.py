c20b0934b653ecb38e926efe089e4520f52b4d71
Quoc An <lincolnan1182005@gmail.com>
2026-01-16 11:24:28 +0700

    database

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:binhnguyenn1208@localhost:5432/omnichat_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:123456789@localhost:5432/omnichat_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
